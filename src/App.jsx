ebccfc25aaa78b8742aa5baa425d37b5a5592432
Rjack00 <Rjackson77@comcast.net>
2025-05-28 18:02:05 -0600

    App.jsx updated
    App functionality and rendering added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { marked } from 'marked';
 import './App.css'
 
+marked.setOptions({
+  breaks: true
+})
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [markdown, setMarkdown] = useState(`# This is the result of your markdown!
+## Here are some examples of markdown:
+1. ***Bold*** and *italic* text
+2. A code block (for multi-line code):
+    \`\`\` 
+    function codeBlock() {
+    return code;
+    } 
+    \`\`\`
+3. Inline code: \`function inlineCode() {return code;}\`
+4. A link: [Google.com](https\\google.com)
+5. A block quote and image:
+    > This is a block quote, and below is an image:
+
+    ![earth](https://upload.wikimedia.org/wikipedia/commons/7/7f/Rotating_earth_animated_transparent.gif)
+`)
+
+ const handleChange = (e) => {
+    setMarkdown(e.target.value);
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="markdown-window">
+          <h2>Markdown Editor</h2>
+          <textarea id='editor' value={markdown} onChange={handleChange}></textarea>
+        </div>
+        <div className="preview-window">
+          <h2>Markdown Preview</h2>
+          <div id='preview' dangerouslySetInnerHTML={{
+          __html: marked(markdown)
+        }} ></div>
+        </div>
     </>
   )
 }
